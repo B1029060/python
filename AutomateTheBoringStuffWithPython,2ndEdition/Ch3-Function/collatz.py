419463b8c9c56f397e0cac1f492a2f690396de0e
B1029060 <B1029060@cgu.edu.tw>
2022-08-14 17:59:26 +0800

    Change in 2022/8/14

--- AutomateTheBoringStuffWithPython,2ndEdition/Ch3-Function/collatz.py
+++ AutomateTheBoringStuffWithPython,2ndEdition/Ch3-Function/collatz.py
@@ -1,19 +1,19 @@
 # About this function
 # if num is odd, return 3 * num + 1, if num was even, then return num / 2
-def Collatz(num: int) -> None:  # Type Annotation
+def collatz(num: int) -> None:  # Type Annotation
     if num >= 2 and num != 1:
         print((new_num := int(3 * num + 1))) if num % 2 == 1 else print((new_num := int(num / 2)))
-        Collatz(new_num)    # Recursive
+        collatz(new_num)    # Recursive
     elif num != 1:
         print('Invalid input')  # print 'Invalid input' when num is integer under 1
 
 try:
     print('Enter number: ')
-    Collatz(int(input()))   # assign int variable type
+    collatz(int(input()))   # assign int variable type
 except ValueError:
     print('Wrong Variable Type')
     print('Please input right variable type: int')
 except RecursionError:
-    print('Infinit Recursion happened!')
+    print('Infinite Recursion happened!')
 finally:
     print('Program Finished')
