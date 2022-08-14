2a336f822e5af8526e9f9c8225af55616a46a2ea
B1029060 <B1029060@cgu.edu.tw>
2022-08-14 17:49:41 +0800

    Change in 2022/8/14

--- AutomateTheBoringStuffWithPython,2ndEdition/Ch3-Function/Collatz.py
+++ AutomateTheBoringStuffWithPython,2ndEdition/Ch3-Function/Collatz.py
@@ -1,7 +1,6 @@
 # About this function
 # if num is odd, return 3 * num + 1, if num was even, then return num / 2
-
-def Collatz(num):
+def Collatz(num: int) -> None:  # Type Annotation
     if num >= 2 and num != 1:
         print((new_num := int(3 * num + 1))) if num % 2 == 1 else print((new_num := int(num / 2)))
         Collatz(new_num)    # Recursive
