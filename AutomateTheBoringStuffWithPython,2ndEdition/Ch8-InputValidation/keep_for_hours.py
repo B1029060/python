479e10f14e1e6b2e3195e1e9581c33f7bc9943c4
B1029060 <B1029060@cgu.edu.tw>
2022-09-08 21:49:00 +0800

    Change in 2022/9/8

--- AutomateTheBoringStuffWithPython,2ndEdition/Ch8-InputValidation/keep_for_hours.py
+++ AutomateTheBoringStuffWithPython,2ndEdition/Ch8-InputValidation/keep_for_hours.py
@@ -10,3 +10,13 @@ while True:
     if (counter := counter + 1) == 100:
         print('You are that idiot!!')
         break
+
+'''
+try:
+    prompt = 'Want to know how to keep an idiot busy for hours?'
+    res = pyin.inputYesNo(prompt=prompt, limit=100, blockRegexes=[(r'[Yy]([Ee][Ss])?', '')])
+    if res == 'no':
+        print('Thank you, have a nice day.')
+except pyin.RetryLimitException:
+    print('You are that idiot!!')
+'''
